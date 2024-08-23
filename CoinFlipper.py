d6e26e5df74e533e1bdf13e317d321400c2c34c2
Zach Potthoff <zach.potthoff@icloud.com>
2024-08-23 15:37:28 -0400

    naming and general conventions

--- CoinFlipper.py
+++ CoinFlipper.py
@@ -1,32 +1,31 @@
 import matplotlib.pyplot as plt
 from BogoCoin import BogoCoin
+from statistics import mean
 
 def main():
     # this function produces a histogram of the different samples
     # of our desired p value, computing the average p
-    wCoin = BogoCoin(0.125)
-    sum = 0
-    avgSum = 0
+    probability = 0.125
+    wCoin = BogoCoin(probability)
     flips = 1000
     samples = 1000
     sums = []
     for i in range(samples):
-        sum = 0
+        heads = 0
         for j in range(flips):
-            sum += wCoin.flip()
-        # print(sum)
-        sums.append(sum/flips)
-        avgSum += sum/flips
+            heads += wCoin.flip()
+        sums.append(heads/flips)
     plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':100})
 
-    avg = avgSum/samples
+    avg = mean(sums)
     print("p_hat is: " + str(avg))
 
     # Plot Histogram on x
-    plt.hist(sums, bins=45)
+    plt.hist(sums, bins=30)
     plt.gca().set(title='P = 0.125 Histogram', ylabel='Frequency')
-    plt.axvline(0.125, color = 'black', linestyle = 'solid', linewidth = 2)
-    plt.axvline(avg, color = 'red', linestyle = 'solid', linewidth = 2)
+    plt.axvline(probability, color = 'black', linestyle = 'solid', linewidth = 2, label = 'p = 0.125')
+    plt.axvline(avg, color = 'red', linestyle = 'solid', linewidth = 2, label = 'p_hat = ' + str(avg))
+    plt.legend()
     plt.show()
 
 main()
